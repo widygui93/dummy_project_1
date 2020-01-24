8f6f082088e340a8cfd449af49157f5bddf93e85
Widy Guilias <guilias.widy@gmail.com>
2020-01-24 23:31:32 +0700

    tambahkan paramater username di searchactv, activity dan editactv

--- ajax/editActv.php
+++ ajax/editActv.php
@@ -21,12 +21,12 @@ $activity = query("SELECT * FROM activity WHERE user_name = '$user' ORDER BY id 
     <?php $no = 1; ?>
     <?php foreach( $activity as $act ) : ?>
         <tr>
-            <td <?= checkActv($act["id"]); ?> ><?= $no; ?></td>
-            <td <?= checkActv($act["id"]); ?> ><?= $act["activity_name"]; ?></td>
+            <td <?= checkActv($act["id"], $act["user_name"]); ?> ><?= $no; ?></td>
+            <td <?= checkActv($act["id"], $act["user_name"]); ?> ><?= $act["activity_name"]; ?></td>
             <td>
-                <a href="#" class="badge badge-primary <?= checkDisableLink($act["id"]); ?> ">Edit</a>
+                <a href="#" class="badge badge-primary <?= checkDisableLink($act["id"], $act["user_name"]); ?> ">Edit</a>
                 <a href="php/hapus.php?id=<?= $act["id"]; ?>" class="badge badge-danger" onclick="return confirm('Do you want to delete this?');">Delete</a>
-                <a href="php/doneActv.php?id=<?= $act["id"]; ?>" class="badge badge-success <?= checkDisableLink($act["id"]); ?> ">Done</a>
+                <a href="php/doneActv.php?id=<?= $act["id"]; ?>" class="badge badge-success <?= checkDisableLink($act["id"], $act["user_name"]); ?> ">Done</a>
             </td>
         </tr>
         <?php $no++; ?>
--- ajax/searchActv.php
+++ ajax/searchActv.php
@@ -18,12 +18,12 @@ $activity = query("SELECT * FROM activity WHERE activity_name LIKE '%$actv_by_se
     <?php $no = 1; ?>
     <?php foreach( $activity as $act ) : ?>
         <tr>
-            <td <?= checkActv($act["id"]); ?> ><?= $no; ?></td>
-            <td <?= checkActv($act["id"]); ?> ><?= $act["activity_name"]; ?></td>
+            <td <?= checkActv($act["id"], $act["user_name"]); ?> ><?= $no; ?></td>
+            <td <?= checkActv($act["id"], $act["user_name"]); ?> ><?= $act["activity_name"]; ?></td>
             <td>
-                <a href="#" class="badge badge-primary <?= checkDisableLink($act["id"]); ?> ">Edit</a>
+                <a href="#" class="badge badge-primary <?= checkDisableLink($act["id"], $act["user_name"]); ?> ">Edit</a>
                 <a href="php/hapus.php?id=<?= $act["id"]; ?>" class="badge badge-danger" onclick="return confirm('Do you want to delete this?');">Delete</a>
-                <a href="php/doneActv.php?id=<?= $act["id"]; ?>" class="badge badge-success <?= checkDisableLink($act["id"]); ?> ">Done</a>
+                <a href="php/doneActv.php?id=<?= $act["id"]; ?>" class="badge badge-success <?= checkDisableLink($act["id"], $act["user_name"]); ?> ">Done</a>
             </td>
         </tr>
         <?php $no++; ?>
